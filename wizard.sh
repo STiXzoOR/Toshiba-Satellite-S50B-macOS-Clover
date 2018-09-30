c230862befaa7074f21be10c04d8d03abdec20e9
Neoptolemos Kyriakou <neostixz@gmail.com>
2018-09-30 16:41:59 +0300

    fixed minor issues

--- wizard.sh
+++ wizard.sh
@@ -4,7 +4,7 @@ repo_dir=$(dirname ${BASH_SOURCE[0]})
 
 macos_tools=$repo_dir/macos-tools
 
-if [[ ! -d $macos_tools || -d $macos_tools ]]; then
+if [[ ! -d $macos_tools ]]; then
     echo "Downloading latest macos-tools..."
     rm -Rf $macos_tools && git clone https://github.com/the-braveknight/macos-tools $macos_tools --quiet
 fi
